d11d9c080d62610d1ac4d6b3a2b10d8f0a44a08e
kishoreB <krrish.kishore02@gmail.com>
2019-03-17 03:33:48 +0530

    updated

---
+++ force-app/main/default/lwc/accountListEdit/accountListEdit.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+import { LightningElement, wire, track, api } from 'lwc';
+import getAccounts from '@salesforce/apex/accList.getAccounts';
+import { refreshApex } from '@salesforce/apex';
+
+export default class AccountListEdit extends LightningElement {
+    @wire(getAccounts) Accounts;
+    @track open = false;
+    @api rec2Id;
+
+    renderedCallback() {
+        console.log("Accounts:::", this.Accounts);
+        //console.log("Accounts:::", JSON.stringify(this.Accounts));
+    }
+
+    handleClick(event) {
+        console.log("In HandleClick");
+        const recId = event.target.name;
+        this.rec2Id = event.currentTarget.name;
+        console.log("Selected Account Id:::", recId);
+        console.log("Selected Account Id rec2Id :::", this.rec2Id);
+        this.open = true;
+    }
+
+    closeModal() {
+        console.log("In closeModal");
+        this.open = false;
+    }
+
+    reloadList() {
+        return refreshApex(this.Accounts);
+    }
+}
